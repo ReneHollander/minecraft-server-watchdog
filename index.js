69f5f339caf2f054020896155aa9b751667603ee
ReneHollander <rene.hollander@hotmail.de>
2015-07-11 16:06:23 +0200

    still fixing executable

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env node
-
 var optimist = require('optimist');
 var watchdog = require("./lib/watchdog.js");
 
-var argv = optimist.argv;
+module.exports = function() {
+  var argv = optimist.argv;
 
-var config = {
-  configfile: argv.config ? argv.config : "watchdog.config.json"
+  var config = {
+    configfile: argv.config ? argv.config : "watchdog.config.json"
+  }
+
+  watchdog(config);
 }
-
-watchdog(config);
